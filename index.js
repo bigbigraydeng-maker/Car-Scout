f6ba9becfbfab9b6fb133b7dd91efdf17b9da688
Deployer <deploy@carscout.com>
2026-02-28 10:29:11 +1300

    Fix Render deployment: Add root index.js entry point

---
+++ index.js
@@ -0,0 +1,2 @@
+// Render 入口文件 - 指向实际的 web-server.js
+require('./src/web-server.js');
--- temp-clean.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const authFile = path.join(__dirname, 'auth', 'facebook_auth.json');
-if (fs.existsSync(authFile)) {
-  fs.unlinkSync(authFile);
-  console.log('Removed old auth');
-}
